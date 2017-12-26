d127b7cca7a2407c9476da334ba97d70932beb3d
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-12-27 03:04:43 +0530

    initial

---
+++ script.js
@@ -0,0 +1,43 @@
+// // alert("hello");
+// var doc = document.querySelector(".container");
+// for(var i=0;i<100;i++){
+
+// // doc.classList.add(".box");
+// doc.className += " " + "box";
+// }
+// var dom = document.querySelector(".box");
+// for(var i=0;i<50;i++){
+// 	dom.style.background = "#ffffff";
+	
+// }
+// for(var i=50;i<100;i++){
+// 	dom.style.background = "#fff65a";
+	
+// }
+
+// var color = $(".menu").
+// $( function() {
+//     $( "#menu" ).menu();
+//   } );
+drawSheet();
+function drawSheet(){
+
+for (i=0; i < 16; i++){
+   			$('<div>').addClass('square-line').appendTo(".container");
+   		};
+for (j=0; j < 16; j++){
+    		$('<div>').addClass('square').css("width",600/16).css("height",600/16).appendTo(".square-line");
+		};
+
+
+}
+
+$('#colors').on('change',function(){
+	var color = $('#colors option:selected').val();
+	switch(color){
+		case 'black':
+		$('.container').on('click',function(){
+			$(this).css("background-color",color = 'black');
+		});
+	}
+});
