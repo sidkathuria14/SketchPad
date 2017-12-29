ed6d4c08be36673bd3cba4e6269939bcf663a935
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-12-30 01:10:34 +0530

    color picker added

--- script.js
+++ script.js
@@ -22,22 +22,36 @@
 drawSheet();
 function drawSheet(){
 
-for (i=0; i < 25; i++){
+for (i=0; i < 16; i++){
    			$('<div>').addClass('square-line').appendTo(".container");
    		};
-for (j=0; j < 25; j++){
-    		$('<div>').addClass('square').css("width",25).css("height",25).appendTo(".square-line");
+for (j=0; j < 16; j++){
+    		$('<div>').addClass('square').css("width",600/16).css("height",600/16).appendTo(".square-line");
 		};
 
 
 }
 
-$('#colors').on('change',function(){
-	var color = $('#colors option:selected').val();
-	switch(color){
-		case 'black':
-		$('.container').on('click',function(){
-			$(this).css("background-color",color = 'black');
-		});
-	}
-});
+
+// $('#colors').on('change',function(){
+// 	var color = $('#colors option:selected').val();
+// 	switch(color){
+// 		case 'black':
+// 		$('.square').on('click',function(){
+// 			$(this).css("background-color",color = 'black');
+// 		});
+// 	}
+// });
+var chosencolor;
+ var picker = new CP(document.querySelector('input'));
+// var color = document.querySelector('input');
+    picker.on("change", function(color) {
+        this.target.value = '#' + color;
+        chosencolor = this.target.value;
+        console.log(this.target.value);
+    });
+
+    $('.square').on('click',function(){
+    	console.log("clicked");
+    	$(this).css("background-color",color = chosencolor);
+    });
