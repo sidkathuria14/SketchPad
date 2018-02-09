a185417d247898c4b85d1da773f0bd25626ad2e4
siddharth kathuria <kathuria.siddharth@yahoo.in>
2018-02-10 02:25:02 +0530

    square size changed

--- script.js
+++ script.js
@@ -22,11 +22,11 @@
 drawSheet();
 function drawSheet(){
 
-for (i=0; i < 16; i++){
+for (i=0; i < 128; i++){
    			$('<div>').addClass('square-line').appendTo(".container");
    		};
-for (j=0; j < 16; j++){
-    		$('<div>').addClass('square').css("width",600/16).css("height",600/16).appendTo(".square-line");
+for (j=0; j < 128; j++){
+    		$('<div>').addClass('square').css("width",600/128).css("height",600/128).appendTo(".square-line");
 		};
 
 
@@ -47,14 +47,33 @@ var chosencolor;
 // var color = document.querySelector('input');
     picker.on("change", function(color) {
         this.target.value = '#' + color;
+        
         chosencolor = this.target.value;
+        this.target.value = "";
+        $(this.target).css('background-color',color = chosencolor);
         console.log(this.target.value);
     });
-
-    $('.square').on('click',function(){
-    	console.log("clicked");
-    	$(this).css("background-color",color = chosencolor);
+    $('.square').on('click mouseover', function () {
+        // Do something for both
+  
+        $(this).css("background-color",color = chosencolor);
+   
     });
+
+    // $('.square').live('click hover', function () {
+    //     // common operation
+    //     $(this).css("background-color",color = chosencolor);
+    //    });
+
+    // $('.square').bind('click hover', function () {
+    //     // common operation
+    //     $(this).css("background-color",color = chosencolor);
+    //    });
+
+    // $('.square').on('click',function(){
+    // 	console.log("clicked");
+    // 	$(this).css("background-color",color = chosencolor);
+    // });
 $('#reset').on('click',function(){
 for(var i=0;i<16;i++){
 for(var j=0;j<16;j++){
